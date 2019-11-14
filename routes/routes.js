ec86ebcd8b0c164cdbd20540cad107e16a46ce10
Vicente Castelló <casvicente@gmail.com>
2019-11-13 22:00:27 -0300

    se actualiza webhook

--- routes/routes.js
+++ routes/routes.js
@@ -9,6 +9,7 @@ router.post('/classify/image', watsonVisualRecognitonController.classifyImage);
 router.post('/search/discovery', watsonDiscoveryController.query)
 router.post('/test/webhook/assistant', (req, res) => {
     console.log(req.body);
-    res.send({response: "Hola Webhook!"});
+    let habitaciones = [101,102,103,104]
+    res.send({response: "Hola Webhook!", hotel: req.body.hotel, date:req.body.date, habitacion:habitaciones[0]});
 })
 module.exports = router;
